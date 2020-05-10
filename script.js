b944958c092679c8ed0c215822130291d7555df3
Gabriel <gabriel.bianchessi21@gmail.com>
2020-05-09 23:14:43 -0300

    Modificações no formulário, CSS e JS
    Fiz modificações referentes às animações no formulário

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 
-document
-    .querySelector("button.fat")
-    .addEventListener("click", function(){
-        document.querySelector("#footer")
-        .classList
-        .toggle("hide")
-    })
+function onOff(){
+    document.querySelector("#modal")
+    .classList
+    .toggle("hide")
+}
