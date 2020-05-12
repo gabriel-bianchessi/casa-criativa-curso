aac45dcc32375ffd12a4ed14f04d7dba5c632fb3
Gabriel <gabriel.bianchessi21@gmail.com>
2020-05-12 11:48:07 -0300

    Interatividade
    Modificações na interatividade usando as variáveis do nunjucks no html

--- server.js
+++ server.js
@@ -2,6 +2,38 @@
 const express = require("express");
 const server = express();
 
+
+const ideas = [
+    {
+        img : "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        title : "Cursos de Programação",
+        category: "Estudo",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias suscipit repellendus dolore, harum nostrum fuga facere!",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img : "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+        title : "Exercício",
+        category: "Saúde",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias suscipit repellendus dolore, harum nostrum fuga facere!",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img : "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+        title : "Meditação",
+        category: "Mentalidade",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias suscipit repellendus dolore, harum nostrum fuga facere!",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img : "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+        title : "Karaokê",
+        category: "Diversão em família",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias suscipit repellendus dolore, harum nostrum fuga facere!",
+        url: "https://rocketseat.com.br"
+    }
+]
+
 //confgurar arquivos estáticos
 server.use(express.static("public"))
 
@@ -18,11 +50,28 @@ nunjucks.configure("views", {
 //criei uma rota /
 // e capturo o pedido do cliente para responder
 server.get("/", function(req, res) {
-    return res.render("index.html")
+    
+    //espalhar no array e reverter 
+    const reversedIdeas = [...ideas].reverse();
+
+    //para exibir as duas últimas ideias 
+    let lastIdeas = []
+    for (let idea of reversedIdeas) {
+        if(lastIdeas.length < 2) {
+            lastIdeas.push(idea)
+        }
+    }
+
+    return res.render("index.html", { ideas : lastIdeas})
 })
 
+
+//renderização do ideas
 server.get("/ideias", function(req, res) {
-    return res.render("ideias.html")
+    
+    const reversedIdeas = [...ideas].reverse();
+
+    return res.render("ideias.html", {ideas: reversedIdeas})
 })
 
 //liguei meu servidor na porta 3000
